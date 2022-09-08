c383287c6c3a7855c47d98b1af449bfc42cfb597
Me-RajatChaudhary <111131727+Me-RajatChaudhary@users.noreply.github.com>
2022-09-08 21:28:43 +0530

    Create testbench.sv

---
+++ FSM/SequenceDetector/testbench.sv
@@ -0,0 +1,31 @@
+// Code your testbench here
+// or browse Examples
+module tb_seq_det_mealy;
+  logic clk, clr, x, y;
+  
+  //Instantiate RTL
+  seq_detect_mealy SQDM(.*);
+  
+  initial begin
+    clk=1'b1; clr=1'b1; x=1'b0;    
+    #3 x=1'b0;
+    #3 x=1'b1;
+    #3 x=1'b1;
+    #3 x=1'b0;
+    #3 x=1'b0;
+    #3 x=1'b0;
+    #2 clr=1'b0;
+    #5 $finish;
+  end
+  
+  always #1 clk= ~clk; 
+  
+  //Dump waveforms, monitor
+  initial begin
+    $dumpfile("sqdm.vcd");
+    $dumpvars(0, tb_seq_det_mealy);
+    $monitor($time, " clk = %b, x=%b, y=%b, clr=%b", clk, x, y, clr);
+  end
+    
+endmodule
+  
